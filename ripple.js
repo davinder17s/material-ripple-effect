ff7c28aa27479811b9558d331df785072d2f9f17
Jeremy Holstein <jjjrmy@users.noreply.github.com>
2020-07-01 17:11:31 -0400

    Update ripple.js

--- ripple.js
+++ ripple.js
@@ -16,14 +16,17 @@ RippleEffect.prototype = {
         var rippleContainer = document.createElement('div');
         rippleContainer.style.position = 'absolute';
         rippleContainer.style.zIndex = 99;
-        rippleContainer.style.width = offsetInfo.width + 'px';
-        rippleContainer.style.height = offsetInfo.height + 'px';
         rippleContainer.style.borderRadius = 'inherit';
+        rippleContainer.style.left = 0;
+        rippleContainer.style.right = 0;
+        rippleContainer.style.top = 0;
+        rippleContainer.style.bottom = 0;
+        rippleContainer.style.width = '100%';
+        rippleContainer.style.height = '100%';
         rippleContainer.className = 'ripple-container';
         rippleContainer.style.overflow = 'hidden';
         this.element.appendChild(rippleContainer);
-        
-         // fixed the bug
+
         var maxLength = offsetInfo.width > offsetInfo.height ? offsetInfo.width : offsetInfo.height;
         var circleD = maxLength * 2;
 
@@ -31,7 +34,7 @@ RippleEffect.prototype = {
         ripple.style.position = 'absolute';
         ripple.style.width = circleD + 'px';
         ripple.style.height = circleD + 'px';
-        ripple.style.borderRadius = '500px';
+        ripple.style.borderRadius = '9999px';
         ripple.style.left = ((event.pageX - offsetInfo.left) - circleD/2) + 'px';
         ripple.style.top = ((event.pageY - offsetInfo.top) - circleD/2) + 'px';
         ripple.style.animation = 'ripple 2s forwards cubic-bezier(0, 0, 0.2, 1)';
